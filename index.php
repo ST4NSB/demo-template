c603c6107ffa06d3393f08cac771d9e80fa683ba
ST4NSB <barbulescuadrian10@gmail.com>
2019-10-19 20:34:52 +0300

    added nav and header,footer, div view tags

--- index.php
+++ index.php
@@ -5,15 +5,35 @@
     <title>Main Page</title>
   </head>
   <body>
-    
+  
     <?php
-      session_start();
-      if(!$_SESSION['$user_logged']) {
-        include "form_login.html"; 
-        include "form_register.html";
-      }
-      else echo "<p>welcome</p>";
+      include 'navbar.php'
     ?>
+    
+    <header>
+      <h1>Header tag</h1>
+    </header>
+    
+    <div id="text_view">
+      <div id="left_view">
+        <h3>Left view</h3>
+        <?php
+          session_start();
+          if(!$_SESSION['$user_logged']) {
+            include "form_login.html"; 
+            include "form_register.html";
+          }
+          else echo "<p>welcome</p>";
+        ?>
+      </div>
+      <div id="right_view">
+        <h3>Right view</h3>
+      </div>  
+    </div>
+    
+    <footer>
+      <h2>Footer tag</h2>
+    </footer>
 
   </body>
 </html>
---
+++ navbar.php
@@ -0,0 +1,5 @@
+<ul>
+  <li><a href="index.php">Index</a></li>
+  <li><a href="2.php">Pag. 2</a></li>
+  <li><a href="3.php">Pag. 3</a></li>
+</ul>
