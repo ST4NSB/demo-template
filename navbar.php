d9ba9ac619a5e8e2bddd395ddd03d1147d7a9711
ST4NSB <barbulescuadrian10@gmail.com>
2019-10-28 18:53:19 +0200

    changed page permissions

--- navbar.php
+++ navbar.php
@@ -1,7 +1,12 @@
 <nav>
   <ul>
     <li><a href="index.php">Index</a></li>
-    <li><a href="2.php">Pag. 2</a></li>
-    <li><a href="3.php">Pag. 3</a></li>
+		<?php
+		include 'php/user_logged.php';
+		if($_SESSION['$user_logged']) {
+			echo '<li><a href="2.php">Pag. 2</a></li>';
+			echo '<li><a href="3.php">Pag. 3</a></li>';
+		}
+		?>
   </ul>
 </nav>
