f906002f4ceeca626ece52f9907084478884fd10
Bărbulescu Adrian <38291834+ST4NSB@users.noreply.github.com>
2019-10-28 18:57:53 +0200

    tabbed data

--- navbar.php
+++ navbar.php
@@ -2,11 +2,11 @@
   <ul>
     <li><a href="index.php">Index</a></li>
 		<?php
-		include 'php/user_logged.php';
-		if($_SESSION['$user_logged']) {
-			echo '<li><a href="2.php">Pag. 2</a></li>';
-			echo '<li><a href="3.php">Pag. 3</a></li>';
-		}
+			include 'php/user_logged.php';
+			if($_SESSION['$user_logged']) {
+				echo '<li><a href="2.php">Pag. 2</a></li>';
+				echo '<li><a href="3.php">Pag. 3</a></li>';
+			}
 		?>
   </ul>
 </nav>
